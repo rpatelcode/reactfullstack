022b1f67c05f8e1cd4edbc301daab43528194fe0
Ramesh Patel <rameshbhai.patel@toronto.ca>
2018-07-13 13:56:56 -0400

    changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
   app.get('/', (req, res) => {
-    res.send({ hi : "Lala land"});
+    res.send({ hi : "Lala land 2"});
   });
 
 const PORT = process.env.PORT || 5000;
