41ef89f89c548b2a2e6edae600eb15d68fe71de1
Ramesh Patel <rameshbhai.patel@toronto.ca>
2018-07-14 08:01:31 -0400

    changes

--- config/prod.js
+++ config/prod.js
@@ -3,8 +3,8 @@
 
 
 module.exports = {
-  googleClientID: "774299394318-2vs0bq13ntpp2t0tljf2608a8nnfpdgc.apps.googleusercontent.com",
-  googleClientSecret: "bXETj6ASSTp2kkTNDuT-mhRY",
+  googleClientID: "",
+  googleClientSecret: "",
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
