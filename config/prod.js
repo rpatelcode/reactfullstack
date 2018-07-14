8cde6b53a0c474efceabe9cc8ad085b94ffdf73d
Ramesh Patel <rameshbhai.patel@toronto.ca>
2018-07-14 07:53:48 -0400

    change

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,20 @@
 // prod.js - production keys here!!
+
+// {"web":{
+//   "client_id":"774299394318-2vs0bq13ntpp2t0tljf2608a8nnfpdgc.apps.googleusercontent.com",
+//   "project_id":"emaily-dev-210118",
+//   "auth_uri":"https://accounts.google.com/o/oauth2/auth",
+//   "token_uri":"https://accounts.google.com/o/oauth2/token",
+//   "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
+//   "client_secret":"bXETj6ASSTp2kkTNDuT-mhRY",
+//   "redirect_uris":["http://localhost:5000/*"],
+//   "javascript_origins":["http://localhost:5000"]
+//   }}
+
+
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  googleClientID: "774299394318-2vs0bq13ntpp2t0tljf2608a8nnfpdgc.apps.googleusercontent.com",
+  googleClientSecret: "bXETj6ASSTp2kkTNDuT-mhRY",
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
