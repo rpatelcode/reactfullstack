811db4cb39ecb972dd9577e28d40c6c409ef1a8d
Ramesh Patel <rameshbhai.patel@toronto.ca>
2018-07-14 07:54:42 -0400

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,15 +1,5 @@
 // prod.js - production keys here!!
 
-// {"web":{
-//   "client_id":"774299394318-2vs0bq13ntpp2t0tljf2608a8nnfpdgc.apps.googleusercontent.com",
-//   "project_id":"emaily-dev-210118",
-//   "auth_uri":"https://accounts.google.com/o/oauth2/auth",
-//   "token_uri":"https://accounts.google.com/o/oauth2/token",
-//   "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
-//   "client_secret":"bXETj6ASSTp2kkTNDuT-mhRY",
-//   "redirect_uris":["http://localhost:5000/*"],
-//   "javascript_origins":["http://localhost:5000"]
-//   }}
 
 
 module.exports = {
