6a888644990c9c63c3e0adaf7b42a7edd066cf8f
Ramesh Patel <rameshbhai.patel@toronto.ca>
2018-07-18 10:30:15 -0400

    TEST

--- config/prod.js
+++ config/prod.js
@@ -6,14 +6,12 @@
   // "auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs",
   // "client_secret":"0d43RzQkmhwopw02eCpqeWg0"
 
-
-
 // prod.js - production keys here!!
 module.exports = {
-  googleClientID: '253143829911-efj0dnjk8v8i2t4uka9lm1jkhehd9m1b.apps.googleusercontent.com',
-  googleClientSecret: '0d43RzQkmhwopw02eCpqeWg0',
-  mongoURI: 'mongodb://uemailyprod:2pemailyprod@ds141661.mlab.com:41661/emaily-prod9',
-  cookieKey: 'anythingyoucantypehere',
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
   stripeSecretKey: process.env.STRIPE_SECRET_KEY,
   sendGridKey: process.env.SEND_GRID_KEY,
